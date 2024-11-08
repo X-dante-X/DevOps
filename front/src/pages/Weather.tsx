1fd5bddbe32829464c69e2436f08b8a007e77b36
Yevhenii Solomchenko <73559936+X-dante-X@users.noreply.github.com>
2024-11-08 13:06:34 +0100

    add mongoDB

--- front/src/pages/Weather.tsx
+++ front/src/pages/Weather.tsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react';
+
 function Weather() {
-    return (<>
-    <h1>Weather</h1>
-    </>  );
+    const [weatherData, setWeatherData] = useState([]);
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            const response = await fetch('http://localhost:5051/api/weather');
+            if (response.ok) {
+                const data = await response.json();
+                setWeatherData(data);
+            } else {
+                console.error('Failed to fetch weather data');
+            }
+        };
+
+        fetchWeatherData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Weather Data</h1>
+            {weatherData.length > 0 ? (
+                <ul>
+                    {weatherData.map((weather, index) => (
+                        <li key={index}>
+                            <strong>Date:</strong> {weather.date} - <strong>Temp:</strong> {weather.temperatureC}°C - <strong>Summary:</strong> {weather.summary}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No weather data available.</p>
+            )}
+        </div>
+    );
 }
 
 export default Weather;
