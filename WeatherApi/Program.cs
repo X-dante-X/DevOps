3537f5fd2203483c948b61aabcf636156afa0a0f
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2024-11-13 11:05:15 +0100

    auto instrumentation metrics

--- WeatherApi/Program.cs
+++ WeatherApi/Program.cs
@@ -4,13 +4,13 @@ using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
- builder.Services.AddOpenTelemetry()
-                    .WithMetrics(metrics => metrics
-                        .AddAspNetCoreInstrumentation()
-                        .AddRuntimeInstrumentation()
-                        .AddHttpClientInstrumentation()
-                        .AddProcessInstrumentation()
-                        .AddPrometheusExporter());
+//  builder.Services.AddOpenTelemetry()
+//                     .WithMetrics(metrics => metrics
+//                         .AddAspNetCoreInstrumentation()
+//                         .AddRuntimeInstrumentation()
+//                         .AddHttpClientInstrumentation()
+//                         .AddProcessInstrumentation()
+//                         .AddPrometheusExporter());
 
 var connectionUri = Environment.GetEnvironmentVariable("MONGODB_URI");
 var settings = MongoClientSettings.FromConnectionString(connectionUri);
@@ -38,7 +38,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseOpenTelemetryPrometheusScrapingEndpoint();
+// app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
