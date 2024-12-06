61e57617f2041c0230775b5a040a95a0ea19f022
Yevhenii Solomchenko <73559936+X-dante-X@users.noreply.github.com>
2024-12-06 12:56:26 +0100

    all

--- runk8s.ps1
+++ runk8s.ps1
@@ -1,10 +1,10 @@
 $manifestsPath = "./kubernetes"
 
 kubectl delete pods --all -n default
-
 kubectl delete svc --all
 
 docker compose build
+docker-compose build
 
 Get-ChildItem -Path $manifestsPath -Filter *.yaml | ForEach-Object {
     kubectl apply -f $_.FullName
--- runk8s.sh
+++ runk8s.sh
@@ -1,10 +1,11 @@
 manifests_path="./kubernetes"
 
 kubectl delete pods --all -n default
-
-kubectl delete svc --all
+kubectl delete svc --all -n default
 
 docker compose build
+docker-compose build
+
 
 for file in "$manifests_path"/*.yaml; do
   kubectl apply -f "$file"
